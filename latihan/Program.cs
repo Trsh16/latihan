da63040c01b73410b1619989d07d03a546ffe45a
Trsh16 <tlikorawung@student.ciputra.ac.id>
2021-12-10 12:06:58 +0800

    1

--- latihan/Program.cs
+++ latihan/Program.cs
@@ -10,34 +10,8 @@ namespace latihan
     {
         public static void main(String[] args)
         {
-            int n = 256;
-            int[] total = new int[n];
             Console.WriteLine("INPUT KALIMAT : ");
-            var kata = Console.ReadLine();
-            List<int> KaTa = new List<int>();
-            for (int i = 0; i < n; i++)
-            {
-                total[i] = 0;
-            }
-            int katakata = kata.Length;
-            for (int i = 0; i < katakata; i++)
-            {
-                total[(int)(char)i]++;
-            }
-            Console.WriteLine();
-            for (int i = 0; i < n; i++)
-            {
-                if (total[i] > 0)
-                {
-                    if (((char)i >= 'a' && (char)i <= 'z'))
-                    {
-                        Console.WriteLine((char)i + " = " + total[i] + " HURUF");
-                    }
-                    KaTa.Add(total[i]);
-                }
-            }
-            Console.WriteLine(kata + " = " +KaTa);
-            Console.ReadKey();
+            var kata = Console.ReadLine();         
         }
     }
 }
